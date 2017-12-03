a80ca80b4921666dbeb596212f870d91e8bb63b8
Rene Krenn <rkrenn@phoenixctms.org>
2017-12-03 18:36:46 +0100

    renew IP address

--- install.sh
+++ install.sh
@@ -3,6 +3,9 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
+###renew ip:
+dhclient -v
+
 ###install some general packages:
 #apt-get -y install mc open-vm-tools net-tools
 apt-get -y install sudo wget ca-certificates
